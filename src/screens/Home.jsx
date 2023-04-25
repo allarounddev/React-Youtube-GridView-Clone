3c86c9a56a91aca433ceb0d896c76e22d19f2f0e
full stack <allarounddeveloper@outlook.com>
2023-04-25 23:52:55 +0000

    Update 5 files

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,44 +1,40 @@
-import { View,StyleSheet} from 'react-native'
-import React from 'react'
-import Navbar  from '../components/Navbar';
-import Dashboard from '../components/Dashboard';
-import SideBar from '../components/SideBar';
+import { View, StyleSheet } from "react-native";
+import React from "react";
+import Dashboard from "../components/Dashboard";
 const Home = () => {
-  
   return (
     <View style={[styles.HOME, { backgroundColor: "white" }]}>
-        {/* <Navbar/> */}
-        <View style={styles.Home_container}>
-        {/* <View style={styles.Home_container_sidebar}><SideBar/></View> */}
-        <View style={styles.Home_container_Dashboard}><Dashboard/></View>
-
+      {/* <Navbar/> */}
+      <View style={styles.Home_container}>
+        <View style={styles.Home_container_Dashboard}>
+          <Dashboard />
         </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    HOME:{
-        flex:1,
-      width:"100%",
-      height:"100vh",
-    },
-    Home_container:{
-      flex:1,
-      display:"flex",
-      flexDirection:"row",
-      width:"100%",
-      height:"100%",
-    },
-    Home_container_sidebar:{
-      width:320,
-      height:"100%",
-    },
-    Home_container_Dashboard:{
-      width:"100%",
-      flex:1,
-      height:"100%",
-    }
-
-})
-export default Home
+  HOME: {
+    flex: 1,
+    width: "100%",
+    height: "100vh",
+  },
+  Home_container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+  },
+  Home_container_sidebar: {
+    width: 320,
+    height: "100%",
+  },
+  Home_container_Dashboard: {
+    width: "100%",
+    flex: 1,
+    height: "100%",
+  },
+});
+export default Home;
